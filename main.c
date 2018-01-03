d2c6934be33019009be7ea9b3f872f19b655a51d
Victor MERCIER <victor.mercier@outlook.com>
2018-01-03 15:16:16 +0100

    Adding condition in order to check the input file.

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-
 int main()
 {
     int fd;
     int i;
 
-    fd = _open("input.txt", O_RDONLY, 0);
+    fd = open("input.txt", O_RDONLY, 0);
+    i = lseek(fd, 0, SEEK_END);
+    close(fd);
 
-    i = _seek(fd, 0, SEEK_END);
-    printf("size = %d\n", i);
-
-    _close(fd);
+    if (i=-1)
+        printf("The file isn't correct\n");
+    else
+        printf("size = %d\n", i);
     return 0;
 }
