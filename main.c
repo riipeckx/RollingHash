5ff126cbdc82ab3766b8c3dcf6e0303fd131b229
Victor MERCIER <riip3ckx@gmail.com>
2018-01-03 17:08:01 +0100

    Version-003 ---------------------------  - Memory Allocation for the given file

--- main.c
+++ main.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
-#define MY_BUFFER_SIZE size
+#include <stdlib.h>
 
-int main(void)
-{
+/*
+ * malloc = Allouer de l'espace mémoire
+ * free = Désallouer de l'espace mémoire
+ */
+
+int main(void) {
     int fd;
     int size;
-    char mybuffer[size];
+    int* allocatedMemory = NULL;
+
+    allocatedMemory = malloc(size * sizeof(int));
 
     fd = open("c:/input.txt", O_RDONLY, 0666);
     size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
-    printf("buffer size = %d\n", mybuffer);
+    printf("Allocated memory = %d\n", allocatedMemory);
 
 //Input file verification method
-    if (size==-1){
+    if (size == -1) {
         printf("The file isn't correct\n");
         printf("err = %s\n", strerror(errno));
-    }
-    else{
-        printf("The file conatins %d caracters.\n", size);
+    } else {
+        printf("The file contains %d characters.\n", size);
     }
 
+    free(allocatedMemory);
     return 0;
 }
