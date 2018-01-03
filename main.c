2c36c7b9758d2d2b6460510ff45507572b6bb675
Victor MERCIER <victor.mercier@outlook.com>
2018-01-03 15:08:11 +0100

    Adding Import_Files code.

--- main.c
+++ main.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <fcntl.h>
+
+
+int main()
+{
+    int fd;
+    int i;
+
+    fd = _open("input.txt", O_RDONLY, 0);
+
+    i = _seek(fd, 0, SEEK_END);
+    printf("size = %d\n", i);
+
+    _close(fd);
+    return 0;
+}
