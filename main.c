6d5df1904c6cbaf2ff3de08519a168e4409d7532
Victor MERCIER <riip3ckx@gmail.com>
2018-01-03 15:52:28 +0100

    Version-001
    ===== Notes =====
    The program should now be able to tell you the length of a given file.

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 
 int main()
 {
     int fd;
     int i;
 
-    fd = open("input.txt", O_RDONLY, 0);
+    fd = open("c:/input.txt", O_RDONLY, 0666);
     i = lseek(fd, 0, SEEK_END);
     close(fd);
 
-    if (i=-1)
+//Input file verification method
+    if (i==-1){
         printf("The file isn't correct\n");
-    else
+        printf("err = %s\n", strerror(errno));
+    }
+    else{
         printf("size = %d\n", i);
+    }
+
     return 0;
 }
