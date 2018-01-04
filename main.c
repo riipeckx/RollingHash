663b603ebfe843d08e3c01f3ecf297eddaa4fbb5
Victor MERCIER <victor.mercier@outlook.com>
2018-01-04 10:33:44 +0100

    Version-007.5 ---------------------------  - Indented main.c

--- main.c
+++ main.c
@@ -4,37 +4,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/*
- * malloc = Allouer de l'espace mémoire
- * free = Désallouer de l'espace mémoire
- */
-
 char* loadFile(char* given_file, int* p_size){
 
     int fd;
     char* allocatedMemory = NULL;
 
     fd = open(given_file, O_RDONLY, 0666);
-
     *p_size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
     allocatedMemory = malloc(*p_size);
     printf("Allocated memory = %p (%d)\n", allocatedMemory, *p_size);
-
     read(fd, allocatedMemory, *p_size);
-
     close(fd);
-
-
-    /*
-    if (size == -1) {
-        printf("The file isn't correct\n");
-        printf("err = %s\n", strerror(errno));
-    } else {
-        printf("The file contains %d characters.\n", size);
-    }*/
-
-    //close
 
     return allocatedMemory;
 };
@@ -45,9 +26,7 @@ int main(void) {
     int i;
 
     buffer = loadFile("c:/input.txt", &size);
-
     printf("%d\n",size);
-
     for(i=0;i<size;i++){
         printf("%c", buffer[i]);
     }
