a6203fe47cc0f24d8a7c05e8d7cfedbca839dc21
Victor MERCIER <riip3ckx@gmail.com>
2018-01-04 09:29:15 +0100

    Version-005 ---------------------------  - Methods method.

--- main.c
+++ main.c
@@ -2,31 +2,55 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*
  * malloc = Allouer de l'espace mémoire
  * free = Désallouer de l'espace mémoire
  */
 
-int main(void) {
-    int fd;
-    int size;
-    char* allocatedMemory = NULL;
+//variables declaration
+int fd;
+int size;
+char* allocatedMemory = NULL;
+char textInBuffer = 0;
 
+void fileOpening(){
     fd = open("c:/input.txt", O_RDONLY, 0666);
+};
+
+void fileSize(){
     size = lseek(fd, 0, SEEK_END);
+};
+
+void memoryAllocation(){
     lseek(fd, 0, SEEK_SET);
     allocatedMemory = malloc(size);
     printf("Allocated memory = %p (%d)\n", allocatedMemory, size);
+};
 
-//Input file verification method
+void memoryDeallocation(){
+    free(allocatedMemory);
+};
+
+void fileVerification(){
     if (size == -1) {
         printf("The file isn't correct\n");
         printf("err = %s\n", strerror(errno));
     } else {
         printf("The file contains %d characters.\n", size);
     }
+};
 
-    free(allocatedMemory);
+int main(void) {
+
+    fileOpening();
+    fileSize();
+
+    memoryAllocation();
+
+    fileVerification();
+
+    memoryDeallocation();
     return 0;
 }
