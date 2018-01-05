b3d2223f0c12b9bfa08e3e0a2f1a56d0616abe82
Victor MERCIER <riip3ckx@gmail.com>
2018-01-05 16:45:42 +0100

    Version 0.1.3 ==============  - The checksum is now displayed as hexadecimal code.

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <stdint.h>
 
-char* loadFile (char* given_file, int* p_size) {
+uint8_t * loadFile (char* given_file, int* p_size) {
     int fd;
-    char *allocatedMemory = NULL;
+    uint8_t *allocatedMemory = NULL;
 
     fd = open(given_file, O_RDONLY, 0666);
     *p_size = lseek(fd, 0, SEEK_END);
@@ -25,7 +27,9 @@ char* loadFile (char* given_file, int* p_size) {
 
         return allocatedMemory;
     };
-}
+};
+
+
 
 void quit (void){
     int i;
@@ -35,11 +39,18 @@ void quit (void){
 
 int main (void) {
     // Variables
-    char *buffer;
+    uint8_t *buffer;
     int size;
     int i;
     int csum;
     int remove[256];
+    int a = csum;
+
+    // Clock
+    clock_t start, stop;
+
+    start = clock();
+
 
     // Pre-Calculating
     for (i=0; i<256;i++){
@@ -48,30 +59,27 @@ int main (void) {
 
 
      // Load File
-     buffer = loadFile("/home/riipeckx/C/RollingHash/input.txt", &size);
+    buffer = loadFile("/home/riipeckx/C/RollingHash/a.out", &size);
+
 
 
      // Rolling Checksum
     csum = 33 * 33 * buffer[0] + 33 * buffer[1] + buffer[2];
-    printf("csum[0] = %d \n", csum);
+    //printf("csum[0] = %d \n", csum);
 
     for (i = 0; i < size - 3; i++) {
-        //csum = csum - 33 * 33 * buffer[i];
         csum = csum - remove[buffer[i]];
         csum = csum * 33;
         csum = csum + buffer[i + 3];
-        printf("csum[%d] = %d \n", i + 1, csum);
+        //printf("csum[%d] = [%d] %d\n", i+1,  (int)buffer[i], csum);
+        printf("0x%X \n", csum);
     }
 
+    stop = clock();
 
-    /*Dynamic Hash Table
-    /*int hashTable[8];
+    printf("time = %f\n", (double)(stop - start) / CLOCKS_PER_SEC);
 
-    for(int i = 0; i<hashTable; i++){
-        hashTable[i] = -1;
-        printf("%d \n", hashTable[]);
-    //}*/
 
     quit();
     return 0;
-}
+};
