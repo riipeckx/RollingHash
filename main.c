c4636cfb9936e11185b37aa3e957de5dea5d854c
Victor MERCIER <victor.mercier@outlook.com>
2018-01-09 17:15:59 +0100

    Version 0.3.1 ==============
     - Commit notes :
    
         - Added a function returning the longest sequence for every pattern in the given file.
         - Code corrections.

--- main.c
+++ main.c
@@ -24,13 +24,11 @@ uint8_t * loadFile (char* given_file, int* p_size){
         printf("Error code is = %s\n", strerror(errno));
     } else {
         printf("The file is correct.\nIt contains %d characters.\n", *p_size);
-
         read(fd, allocatedMemory, *p_size);
         close(fd);
-
         return allocatedMemory;
-    };
-};
+    }
+}
 
 int lSeq (char *sequence, int size, int i, int j){
     if (i>=size || j>=size || sequence[i]!=sequence[j]) return 0;
@@ -41,12 +39,10 @@ void quit (void){
     int i;
     printf("Press any key to continue...");
     read(0, &i, 1);
-};
+}
 
 int main (void) {
-
     // Variables
-    char path;
     uint8_t *buffer;
     int size;
     int i, j;
@@ -61,38 +57,27 @@ int main (void) {
 
     // Clock
     clock_t start, stop;
-
     start = clock();
 
-    // Pre-Calculating
-    for (i=0; i<256;i++){
-        remove[i] = MUL * MUL * i;
-    }
+    // Pre-Calculating MUL
+    for (i=0; i<256;i++) remove[i] = MUL * MUL * i;
 
-     // Load File
+    // Load File
     //buffer = loadFile("input.txt", &size);
     //buffer = loadFile("5MB.zip", &size);
     buffer = loadFile("1GB.zip", &size);
 
-     // Rolling Checksum
+    // Rolling Checksum
     csum = MUL * MUL * buffer[0] + MUL * buffer[1] + buffer[2];
 
     for (i = 0; i < size - 3; i++) {
-
-        //printf("%-5d", i);
-        //for (j = 0; j<HSIZE; j++) printf("%3d", hashTable[j]);
-        //printf(" | %3d (%c%c%c)", csum % HSIZE, buffer[i], buffer[i+1], buffer[i+2]);
-        //printf(" %d\n", lSeq(buffer, size, hashTable[csum % HSIZE], i));
-
         hashTable[csum % HSIZE] = i;
-
         csum -= remove[buffer[i]];
         csum *= MUL;
         csum += buffer[i + 3];
     }
 
     stop = clock();
-
     printf("time = %f\n", (double)(stop - start) / CLOCKS_PER_SEC);
 
     for (i=0; i<HSIZE; i++) printf("%d ", hashTable[i]);
